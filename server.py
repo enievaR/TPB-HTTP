18a39ed2f3d16524988f042b0b969b6e38f33ea5
MORDOHAI Florian <florian.mordohai@etu.univ-amu.fr>
2025-06-17 15:03:31 +0200

    push server

--- server.py
+++ server.py
@@ -3,19 +3,18 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class MonHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        reponse = """HTTP/1.1 200 OK\n
-        Content-Type: text/plain\n
-        Content-Length: 12\n
-        Connection: close\n
-        \n
+        reponse = """HTTP/1.1 200 OK
+        Content-Type: text/plain
+        Content-Length: 12
+        Connection: close
+
         Hello World!"""
         self.wfile.write(reponse.encode())
         # Lancement du serveur sur le port 8000
-        server = HTTPServer(("0.0.0.0", 8000), MonHandler)
-        print("Serveur HTTP actif sur le port 8000")
+        server = HTTPServer(("localhost", 8080), MonHandler)
         server.serve_forever()
 
 
         if __name__ == "__main__":
             MonHandler.do_GET(None)
-            print("Serveur démarré. Accédez à http://localhost:8000/")
+            print("Serveur démarré. Accédez à http://localhost:8080/")
